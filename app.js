bde49f687b345259a34a7ae107872e609f055976
Akshay Ravikumar <akshayravikumar@dhcp-18-111-96-148.dyn.mit.edu>
2015-10-16 12:07:42 -0400

    parentheses fml

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('handlebars/runtime');
 
 // DATABASE CONNECTION
 var mongoose = require('mongoose');
-mongoose.connect((process.env.MONGOLAB_URI  || 'mongodb://localhost/test');
+mongoose.connect(process.env.MONGOLAB_URI  || 'mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
